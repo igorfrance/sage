e72edff4a5885539dabecca2af832d126b093e15
Igor France <igor.france@gmail.com>
2011-12-29 12:32:35 +0100

    Moved the new Sage modules to the DevTools project

---
+++ src/Extensions/DevTools/Modules/Source/SyntaxHighlighterModule.cs
@@ -0,0 +1,11 @@
+﻿namespace Sage.Modules.Source
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+
+	public class SyntaxHighlighterModule
+	{
+	}
+}
---
+++ src/Sage/ResourceManagement/ScriptLibraryInfo.cs
@@ -0,0 +1,51 @@
+﻿namespace Sage.ResourceManagement
+{
+	using System;
+	using System.Diagnostics.Contracts;
+	using System.Xml;
+
+	public class ScriptLibraryInfo
+	{
+		public ScriptLibraryInfo(XmlElement configElem)
+		{
+			Contract.Requires<ArgumentNullException>(configElem != null);
+
+			this.Name = configElem.GetAttribute("name");
+			this.Version = configElem.GetAttribute("version");
+			this.Path = configElem.GetAttribute("path");
+		}
+
+		public string Name
+		{
+			get;
+			private set;
+		}
+
+		public string Version
+		{
+			get;
+			private set;
+		}
+
+		public string Path
+		{
+			get;
+			private set;
+		}
+
+		public XmlElement ToXml(XmlDocument ownerDocument, SageContext context)
+		{
+			Contract.Requires<ArgumentNullException>(ownerDocument != null);
+			Contract.Requires<ArgumentNullException>(context != null);
+
+			string resourcePath = context.Path.Resolve(this.Path);
+
+			XmlElement result = ownerDocument.CreateElement("sage:resource", XmlNamespaces.SageNamespace);
+			result.SetAttribute("type", "script");
+			result.SetAttribute("location", "head");
+			result.SetAttribute("path", context.Path.GetRelativeWebPath(resourcePath));
+
+			return result;
+		}
+	}
+}
