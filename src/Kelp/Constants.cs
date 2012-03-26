1c07e1ccee8ff5f9655d41e9cd2ad2d556575d7a
Igor France <igor.france@gmail.com>
2012-03-26 14:16:49 +0200

    Adapted the relevant XInclude code into Kelp.

--- src/Kelp/Constants.cs
+++ src/Kelp/Constants.cs
@@ -32,6 +32,36 @@ namespace Kelp
 		/// </summary>
 		public const string KelpNamespace = "http://www.cycle99.com/projects/kelp";
 
+		/// <summary>
+		/// Public Xml Namespaces prefix. 
+		/// </summary>
+		/// <remarks>See http://www.w3.org/TR/REC-xml-names/</remarks>
+		public const string XmlNamespacePrefix = "xmlns";
+
+		/// <summary>
+		/// The public XML 1.0 namespace. 
+		/// </summary>
+		/// <remarks>See http://www.w3.org/TR/2004/REC-xml-20040204/</remarks>
+		public const string XmlNamespace = "http://www.w3.org/XML/1998/namespace";
+
+		/// <summary>
+		/// Public Xml Namespaces specification namespace. 
+		/// </summary>
+		/// <remarks>See http://www.w3.org/TR/REC-xml-names/</remarks>
+		public const string XmlNamespacesNamespace = "http://www.w3.org/2000/xmlns/";
+
+		/// <summary>
+		/// XML Schema instance namespace.
+		/// </summary>
+		/// <remarks>See http://www.w3.org/TR/xmlschema-1/</remarks>
+		public const string XsiNamespace = "http://www.w3.org/2001/XMLSchema-instance";
+
+		/// <summary>
+		/// XML 1.0 Schema namespace.
+		/// </summary>
+		/// <remarks>See http://www.w3.org/TR/xmlschema-1/</remarks>
+		public const string XsdNamespace = "http://www.w3.org/2001/XMLSchema";
+
 		private static volatile XmlNamespaceManager nsman;
 
 		/// <summary>
