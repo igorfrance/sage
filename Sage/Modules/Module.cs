6f17ea812be86ecd2b41b14a2b2b4ebadb28e0ab
Igor Francé <igor.france@gmail.com>
2014-06-26 16:31:42 +0200

    Introduced a static class for functionality shared across modules

---
+++ Sage/Modules/Module.cs
@@ -0,0 +1,23 @@
+﻿namespace Sage.Modules
+{
+	using System;
+	using System.Xml;
+
+	/// <summary>
+	/// Provides several shared utility methods for use with Sage modules.
+	/// </summary>
+	public static class Module
+	{
+		/// <summary>
+		/// Gets the configuration text.
+		/// </summary>
+		/// <param name="moduleElement">The module element.</param>
+		/// <param name="xpath">The xpath.</param>
+		/// <returns>System.String.</returns>
+		public static string GetConfigText(XmlNode moduleElement, string xpath)
+		{
+			var node = moduleElement.SelectSingleNode("mod:config/" + xpath, XmlNamespaces.Manager);
+			return node == null ? null : node.InnerText.Trim();
+		}
+	}
+}
