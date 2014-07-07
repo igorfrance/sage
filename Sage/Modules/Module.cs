2274d0610ba062df3d488a04e2f528a90de4f37a
Igor Francé <igor.france@gmail.com>
2014-07-07 10:16:54 +0200

    Easier access to module configuration values.

--- Sage/Modules/Module.cs
+++ Sage/Modules/Module.cs
@@ -13,11 +13,22 @@
 		/// </summary>
 		/// <param name="moduleElement">The module element.</param>
 		/// <param name="xpath">The xpath.</param>
-		/// <returns>System.String.</returns>
+		/// <returns>The text value of the node selected with the specified xpath</returns>
 		public static string GetConfigText(XmlNode moduleElement, string xpath)
 		{
 			var node = moduleElement.SelectSingleNode("mod:config/" + xpath, XmlNamespaces.Manager);
 			return node == null ? null : node.InnerText.Trim();
+		}
+
+		/// <summary>
+		/// Selects the specified nodes within the <c>mod:config</c> section of the specified <paramref name="moduleElement"/>.
+		/// </summary>
+		/// <param name="moduleElement">The module element.</param>
+		/// <param name="xpath">The xpath.</param>
+		/// <returns>The selected nodes</returns>
+		public static XmlNodeList GetConfigNodes(XmlNode moduleElement, string xpath)
+		{
+			return moduleElement.SelectNodes("mod:config/" + xpath, XmlNamespaces.Manager);
 		}
 	}
 }
